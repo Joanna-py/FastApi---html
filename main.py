e27f35b3a911213078a02c73dfc85556a430c081
Joanna Santos <joanna.d.souza@aluno.senai.br>
2025-10-27 11:01:17 -0300

    Priemiro commit do sistema de alunos

---
+++ main.py
@@ -0,0 +1,39 @@
+# pip install fastapi uvicorn jinja2 
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="API de alunos")
+
+#Configurar o diretório dos templates jinja2
+templates = Jinja2Templates(directory="templates")
+
+#Pasta static para servir os arquivos (CSS, Imagens ou JS)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+alunos = [
+    {"nome": "Felipe", "nota": 8.5},
+    {"nome": "Yasmin", "nota": 9.0},
+    {"nome": "Laura", "nota": 8.0},
+    {"nome": "Vitória", "nota": 6.0},
+    {"nome": "Kazuki", "nota": 5.5}
+]
+
+#Rota inicial
+@app.get("/", response_class=HTMLResponse)
+def exibir_alunos(request: Request):
+    return templates.TemplateResponse(
+        "alunos.html", {"request": request, "listar_alunos": alunos}
+    )
+
+@app.get("/cadastro", response_class=HTMLResponse)
+def cadastro_aluno(request: Request):
+    return templates.TemplateResponse(
+        "cadastro.html" , {"request": request}
+        )
+
+@app.post("/cadastro")
+def salvar_aluno(nome: str = Form(...), nota: float = Form(...)):
+    alunos.append({ "nome": nome, "nota": nota} )
+    return RedirectResponse(url="/", status_code=303)
